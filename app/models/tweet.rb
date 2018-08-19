5a20d4dca169803471a5f2d8f6ab57e6471857ac
MateusSchulz <mateus.schulz.m@gmail.com>
2018-08-19 13:12:17 -0300

    Relacionamento de seguidores do twitter criado

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,4 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   validates :content, presence: true
-  
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,27 @@ class User < ApplicationRecord
 
   has_many :tweets
 
+  has_many :active_relationships,  class_name:  "Relationship",
+                                   foreign_key: "follower_id",
+                                   dependent:   :destroy
+
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
+  def following?(other_user)
+  	
+  end
+
+  def follow!(other_user)
+  	
+  end
+
+  def unfollow!(other_user)
+  	
+  end
+
 end
